081515f31db28b3718f86d96ac3789ab576f56e2
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-10-14 06:44:27 +0530

    Add files via upload

---
+++ Keypad_Serial.ino
@@ -0,0 +1,35 @@
+//.........................Jehnakandy..................
+//.....................Arduino Keypad 4x4..............
+//.................Download Keypad.h library...........
+
+#include <Keypad.h>
+
+const byte ROWS = 4; 
+const byte COLS = 4; 
+
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6}; 
+byte colPins[COLS] = {5, 4, 3, 2}; 
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+  char customKey = customKeypad.getKey();
+  
+  if (customKey){
+    Serial.println(customKey);
+  }
+}
+
+//...................Thankyou....................
+//..............coded by : Jehankandy............
